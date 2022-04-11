ee491d618366e58ee4a9e8262992539335c893cd
ruanvalente <ruanopensuselinux@gmail.com>
2022-04-11 15:56:49 -0300

    first commit

---
+++ src/main/java/com/example/tddyoutube/TddYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddYoutubeApplication.class, args);
+	}
+
+}
